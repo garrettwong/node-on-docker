4bba363cdcf38af889a8e635ba9dc7b0655d863f
garrettwong <garrettwong@outlook.com>
2018-07-17 20:13:40 -0700

    deploy new version updated, add material carousel

--- infrastructure/gke/deploy_new_version.sh
+++ infrastructure/gke/deploy_new_version.sh
@@ -3,7 +3,7 @@
 export PROJECT_ID="$(gcloud config get-value project -q)"
 export APP_NAME="nodedocker"
 export DEPLOYMENT_NAME="nodedocker-web"
-export VERSION="v8"
+export VERSION="v10"
 
 docker build -t gcr.io/${PROJECT_ID}/${APP_NAME}:${VERSION} .
 
